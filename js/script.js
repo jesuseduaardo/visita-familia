35dce84d1a84889fe9fcfa6cdfabb1cf07aabdc8
Jesus E Castillo <jesuseduaardo@gmail.com>
2024-04-24 18:33:12 -0300

    Se ajusta para mobile

--- js/script.js
+++ js/script.js
@@ -1,20 +1,29 @@
  // Set the date we're counting down to
- var countDownDate = new Date("Jul 10, 2024 13:37:25").getTime();
+ const countDownDate = new Date("Jul 10, 2024 13:37:25").getTime();
+
+ const subjectList = [
+    "la Sra. Norma y el Sr. Antonio",
+    "Mamá y Papá",
+    "la Abuela y el Abuelo",
+    "la Suegra y el Suegro"
+]
+
+let index = 0;
 
  // Update the count down every 1 second
- var x = setInterval(function () {
+ const x = setInterval(function () {
 
      // Get today's date and time
-     var now = new Date().getTime();
+     const now = new Date().getTime();
 
      // Find the distance between now and the count down date
-     var distance = countDownDate - now;
+     const distance = countDownDate - now;
 
      // Time calculations for days, hours, minutes and seconds
-     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+     const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+     const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+     const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+     const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
      // Output the result in an element with id="demo"
      const left = "<span id='days'>"+days+ " días</span>" + 
@@ -23,9 +32,14 @@
      "<span id='seconds'>" + seconds + " segundos</span>";
      document.getElementById("left").innerHTML = left;
 
+     if(index > (subjectList.length - 1)){
+        index = 0;
+     }
+     document.getElementById("subject").innerHTML = subjectList[index];
+     index++;
      // If the count down is over, write some text 
      if (distance < 0) {
          clearInterval(x);
-         document.getElementById("demo").innerHTML = "EXPIRED";
+         document.getElementById("left").innerHTML = "Ya llegaron!!!";
      }
  }, 1000);
